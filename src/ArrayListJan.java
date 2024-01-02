15b17f39a3fbe09623651f48574739468e57b26f
Sudheer Kumar R Chittireddy <54612677+sudheercreddy@users.noreply.github.com>
2024-01-02 13:15:04 -0600

    Update ArrayListJan.java

--- src/ArrayListJan.java
+++ src/ArrayListJan.java
@@ -3,18 +3,7 @@ public class ArrayListJan {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hello world! its 2024! Wakeup! and get ready for the sameShit!!");
-		System.out.println("Hello world! its 2024! Wakeup! and get ready for the sameShit!!");
-		System.out.println("Hello world! its 2024! Wakeup! and get ready for the sameShit!!");
-		System.out.println("Hello world! its 2024! Wakeup! and get ready for the sameShit!!");
-		System.out.println("Hello world! its 2024! Wakeup! and get ready for the sameShit!!");
-		System.out.println("Hello world! its 2024! Wakeup! and get ready for the sameShit!!");
-		System.out.println("Hello world! its 2024! Wakeup! and get ready for the sameShit!!");
-		System.out.println("Hello world! its 2024! Wakeup! and get ready for the sameShit!!");
-		System.out.println("Hello world! its 2024! Wakeup! and get ready for the sameShit!!");
-		System.out.println("Hello world! its 2024! Wakeup! and get ready for the sameShit!!");
-		System.out.println("Hello world! its 2024! Wakeup! and get ready for the sameShit!!");
-		System.out.println("Hello world! its 2024! Wakeup! and get ready for the sameShit!!");
+		
 
 
 	}
