dd20f9ed94ecab5fd2fab800db823a5c3bd17a54
sudheercreddy <sudheerqa.selenium@gmail.com>
2024-01-02 13:33:06 -0600

    souce indentation

--- src/ArrayListJan.java
+++ src/ArrayListJan.java
@@ -3,9 +3,8 @@ public class ArrayListJan {
 
 	public static void main(String[] args) {
 
-		
-System.out.println("Hello world");
-System.out.println("Adding from  HUB");
+		System.out.println("Hello world");
+		System.out.println("Adding from  HUB");
 		System.out.println("Adding from  HUB");
 	}
 
