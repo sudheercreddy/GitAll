0ed3f0edbfb14350b5f82accf209f3cc8a5d6939
Sudheer Kumar R Chittireddy <54612677+sudheercreddy@users.noreply.github.com>
2024-01-05 12:57:54 -0600

    Userstory5 (#7)
    * Adding to AAA branch
    
    * Adding ArrayList method to other ArrayList
    
    * final example

--- src/Assigmenet/ArrayListPRa.java
+++ src/Assigmenet/ArrayListPRa.java
@@ -5,25 +5,23 @@ import java.util.ArrayList;
 
 public class ArrayListPRa {
 
-	
-	
 	public ArrayList<String> getDevices() {
 		System.out.println("Getting devices");
-		ArrayList<String>users = new ArrayList<String>();
+		ArrayList<String> users = new ArrayList<String>();
 		users.add("shiva");
 		users.add("ravi");
 		users.add("Leena");
 		return users;
-		
+
 	}
+
 	public static void main(String[] args) {
 
 		ArrayListPRa ap = new ArrayListPRa();
-		ArrayList<String> myList= ap.getDevices();
+		ArrayList<String> myList = ap.getDevices();
 		System.out.println(myList.size());
 		myList.add("sudheer");
 		System.out.println(myList.size());
-
 
 	}
 }
---
+++ src/Assigmenet/finalky.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package Assigmenet;
+
+public class finalky {
+
+final static int a =199;
+final static  int NO_OF_DAYS = 7;
+
+	public static void main(String[] args) {
+		finalky nn= new finalky();
+		
+		System.out.println(NO_OF_DAYS);
+	}
+}
